862399d588ef74224d6830098544017de6f19614
Nate Maninger <me@n8m.us>
2023-02-06 20:52:41 -0700

    remove Context field from Client

--- client.go
+++ client.go
@@ -7,7 +7,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 )
 
@@ -15,19 +14,13 @@ import (
 type Client struct {
 	BaseURL  string
 	Password string
-
-	ctx context.Context
 }
 
-func (c *Client) req(method string, route string, data, resp interface{}) error {
+func (c *Client) req(ctx context.Context, method string, route string, data, resp interface{}) error {
 	var body io.Reader
 	if data != nil {
 		js, _ := json.Marshal(data)
 		body = bytes.NewReader(js)
-	}
-	ctx := context.Background()
-	if c.ctx != nil {
-		ctx = c.ctx
 	}
 	req, err := http.NewRequestWithContext(ctx, method, fmt.Sprintf("%v%v", c.BaseURL, route), body)
 	if err != nil {
@@ -41,10 +34,10 @@ func (c *Client) req(method string, route string, data, resp interface{}) error 
 	if err != nil {
 		return err
 	}
-	defer io.Copy(ioutil.Discard, r.Body)
+	defer io.Copy(io.Discard, r.Body)
 	defer r.Body.Close()
 	if !(200 <= r.StatusCode && r.StatusCode < 300) {
-		err, _ := ioutil.ReadAll(r.Body)
+		err, _ := io.ReadAll(r.Body)
 		return errors.New(string(err))
 	}
 	if resp == nil {
@@ -54,29 +47,48 @@ func (c *Client) req(method string, route string, data, resp interface{}) error 
 }
 
 // GET performs a GET request, decoding the response into r.
-func (c *Client) GET(route string, r interface{}) error { return c.req("GET", route, nil, r) }
+func (c *Client) GET(route string, r interface{}) error {
+	return c.req(context.Background(), "GET", route, nil, r)
+}
 
 // POST performs a POST request. If d is non-nil, it is encoded as the request
 // body. If r is non-nil, the response is decoded into it.
-func (c *Client) POST(route string, d, r interface{}) error { return c.req("POST", route, d, r) }
+func (c *Client) POST(route string, d, r interface{}) error {
+	return c.req(context.Background(), "POST", route, d, r)
+}
 
 // PUT performs a PUT request, encoding d as the request body.
-func (c *Client) PUT(route string, d interface{}) error { return c.req("PUT", route, d, nil) }
+func (c *Client) PUT(route string, d interface{}) error {
+	return c.req(context.Background(), "PUT", route, d, nil)
+}
 
 // DELETE performs a DELETE request.
-func (c *Client) DELETE(route string) error { return c.req("DELETE", route, nil, nil) }
+func (c *Client) DELETE(route string) error {
+	return c.req(context.Background(), "DELETE", route, nil, nil)
+}
+
+// GET performs a GET request, decoding the response into r.
+func (c *Client) GETContext(ctx context.Context, route string, r interface{}) error {
+	return c.req(ctx, "GET", route, nil, r)
+}
+
+// POST performs a POST request. If d is non-nil, it is encoded as the request
+// body. If r is non-nil, the response is decoded into it.
+func (c *Client) POSTContext(ctx context.Context, route string, d, r interface{}) error {
+	return c.req(ctx, "POST", route, d, r)
+}
+
+// PUT performs a PUT request, encoding d as the request body.
+func (c *Client) PUTContext(ctx context.Context, route string, d interface{}) error {
+	return c.req(ctx, "PUT", route, d, nil)
+}
+
+// DELETE performs a DELETE request.
+func (c *Client) DELETEContext(ctx context.Context, route string) error {
+	return c.req(ctx, "DELETE", route, nil, nil)
+}
 
 // Custom is a no-op that simply declares the request and response types used by
 // a client method. This allows japecheck to be used on endpoints that do not
 // speak JSON.
 func (c *Client) Custom(method, route string, d, r interface{}) {}
-
-// WithContext returns a copy of the client that uses the provided context for
-// all requests.
-func (c *Client) WithContext(ctx context.Context) *Client {
-	return &Client{
-		BaseURL:  c.BaseURL,
-		Password: c.Password,
-		ctx:      ctx,
-	}
-}
